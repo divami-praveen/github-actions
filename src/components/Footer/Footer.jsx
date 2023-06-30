3d0a7bb5d2811f549129fe19b4e2c5d7ad579cd9
praveen-divami <praveen@divami.com>
2023-06-30 11:41:18 +0530

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,6 @@
 import React from 'react'
 
 const Footer = () => {
-    // some random comment
-    const name = "John Doe"
-    const age = 25
-    const person = {
-        name: "Peter",
-        age: 35,
-        message: "I'm Peter"
-    }
-    const print = () => {
-        console.log("Hello World")
-    }
   return (
     <div>
       
