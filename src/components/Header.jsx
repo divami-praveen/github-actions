d38655edfabcbcfa135414d90d5f6592a83e56e0
praveen-divami <praveen@divami.com>
2023-06-30 11:21:21 +0530

    fix

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+     // JSX
+     const a = 10;
+        const b = 20;
+        const c = a + b;
+        function sum(a, b) {
+            return a + b;
+        }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
